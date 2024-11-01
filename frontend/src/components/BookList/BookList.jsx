0c6dcef1946a9bad5593c7eb9f928ddd34c6f330
Ivan Pakhomov <ip1133@ya.ru>
2024-11-01 23:48:11 +0300

    feat: filter by author

--- frontend/src/components/BookList/BookList.jsx
+++ frontend/src/components/BookList/BookList.jsx
@@ -1,12 +1,16 @@
 import { useSelector, useDispatch } from 'react-redux'
 import { BsBookmarkStarFill, BsBookmarkStar } from 'react-icons/bs'
 import { deleteBook, toggleFavorite } from '../../redux/books/actionCreators'
-import { selectTitleFilter } from '../../redux/slices/filterSlice'
+import {
+  selectTitleFilter,
+  selectAuthorFilter,
+} from '../../redux/slices/filterSlice'
 import './BookList.css'
 
 const BookList = () => {
   const books = useSelector((state) => state.books)
   const titleFilter = useSelector(selectTitleFilter)
+  const authorFilter = useSelector(selectAuthorFilter)
   const dispatch = useDispatch()
 
   const handleDeleteBook = (id) => {
@@ -20,8 +24,10 @@ const BookList = () => {
     const matchesTitle = book.title
       .toLowerCase()
       .includes(titleFilter.toLowerCase())
-
-    return matchesTitle
+    const matchesAuthor = book.author
+      .toLowerCase()
+      .includes(authorFilter.toLowerCase())
+    return matchesTitle && matchesAuthor
   })
 
   return (
@@ -31,25 +37,29 @@ const BookList = () => {
         <p>No books availible</p>
       ) : (
         <ul>
-          {filteredBooks.map((book, i) => (
-            <li key={book.id}>
-              <div className="book-info">
-                {++i}. {book.title} by <strong>{book.author}</strong>
-              </div>
-              <div className="book-actions">
-                <span onClick={() => handleToggleFavourite(book.id)}>
-                  {book.isFavorite ? (
-                    <BsBookmarkStarFill className="star-icon" />
-                  ) : (
-                    <BsBookmarkStar className="star-icon" />
-                  )}
-                </span>
-                <button onClick={() => handleDeleteBook(book.id)}>
-                  Delete
-                </button>
-              </div>
-            </li>
-          ))}
+          {filteredBooks.length === 0 ? (
+            <p>No books for filter</p>
+          ) : (
+            filteredBooks.map((book, i) => (
+              <li key={book.id}>
+                <div className="book-info">
+                  {++i}. {book.title} by <strong>{book.author}</strong>
+                </div>
+                <div className="book-actions">
+                  <span onClick={() => handleToggleFavourite(book.id)}>
+                    {book.isFavorite ? (
+                      <BsBookmarkStarFill className="star-icon" />
+                    ) : (
+                      <BsBookmarkStar className="star-icon" />
+                    )}
+                  </span>
+                  <button onClick={() => handleDeleteBook(book.id)}>
+                    Delete
+                  </button>
+                </div>
+              </li>
+            ))
+          )}
         </ul>
       )}
     </div>
--- frontend/src/redux/slices/filterSlice.js
+++ frontend/src/redux/slices/filterSlice.js
@@ -2,6 +2,7 @@ import { createSlice } from '@reduxjs/toolkit'
 
 const initialState = {
   title: '',
+  author: '',
 }
 
 const filterSlice = createSlice({
@@ -10,13 +11,16 @@ const filterSlice = createSlice({
   reducers: {
     setTitleFilter: (state, action) => {
       state.title = action.payload
-
       // return (state = { // или так
       //   title: action.payload,
       // })
+      // Благодаря immner можно изменять текущее состояние компонента.
+      // return { ...state, title: action.payload }
+      // Традиционный подход.
+    },
 
-      // state.title = action.payload // Благодаря immner можно изменять текущее состояние компонента.
-      // return { ...state, title: action.payload } // Традиционный подход.
+    setAuthorFilter: (state, action) => {
+      state.author = action.payload
     },
 
     resetFilters: (state) => {
@@ -25,8 +29,10 @@ const filterSlice = createSlice({
   },
 })
 
-export const { setTitleFilter, resetFilters } = filterSlice.actions
+export const { setTitleFilter, setAuthorFilter, resetFilters } =
+  filterSlice.actions
 
 export const selectTitleFilter = (state) => state.filter.title
+export const selectAuthorFilter = (state) => state.filter.author
 
 export default filterSlice.reducer
