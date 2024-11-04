fe05a9e3b41f47dae7f6a1ba49405c16169b20e5
Ivan Pakhomov <ip1133@ya.ru>
2024-11-04 20:07:36 +0300

    feat: error slice

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit'
 import booksReducer from './slices/booksSlice'
 import filterReducer from './slices/filterSlice'
+import errorReducer from './slices/errorSlices'
 
 const store = configureStore({
   reducer: {
     books: booksReducer,
     filter: filterReducer,
+    error: errorReducer,
   },
 })
 
